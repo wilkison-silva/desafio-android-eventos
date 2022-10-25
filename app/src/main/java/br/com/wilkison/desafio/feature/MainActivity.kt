83dadc9b2b9c63f712112420ff02598d2fedd660
Wilkison Silva <wilkisons@ciandt.com>
2022-10-25 19:54:32 -0400

    first commit

---
+++ app/src/main/java/br/com/wilkison/desafio/feature/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.wilkison.desafio.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.wilkison.desafio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/br/com/wilkison/desafio/feature/list_events/ListEventsViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.wilkison.desafio.feature.list_events
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+class ListEventsViewModel() : ViewModel() {
+
+
+    fun teste() {
+
+    }
+}
